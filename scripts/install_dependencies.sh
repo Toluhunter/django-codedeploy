1e4a29b7fb93926d775881d4c51aa2be90931f90
Toluhunter <toluhunterdev@gmail.com>
2025-01-18 18:59:34 +0100

    editied appsec

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "Installing Dependencies"
-apt-get update
-apt-get install python3
-ls /var
+ls /
 ls /var/www
 ls /var/www/django-app
 pip3 install -r /var/www/django-app/requirements.txt
--- scripts/start_gunicorn.sh
+++ scripts/start_gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting Gunicorn..."
-cd /var/www/django-app
+cd /django-app
 gunicorn --workers 3 --bind 0.0.0.0:80 django_codedeploy.wsgi:application --daemon
