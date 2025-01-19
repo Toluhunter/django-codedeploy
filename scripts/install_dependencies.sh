92f29a74db90f8d8689840d196c4cd64c46eed35
Toluhunter <toluhunterdev@gmail.com>
2025-01-19 19:40:44 +0100

    added application start

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo "Installing Dependencies"
-pwd
-ls /var
 pip3 install -r /var/www/django-app/requirements.txt
--- scripts/start_gunicorn.sh
+++ scripts/start_gunicorn.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 echo "Starting Gunicorn..."
 cd /django-app
+python3 -m venv venv
+source venv/bin/activate
+
+pip3 install -r requirements.txt
+
 gunicorn --workers 3 --bind 0.0.0.0:80 django_codedeploy.wsgi:application --daemon
